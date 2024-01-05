7ba31e947afa7963f0f94d67f990c4fb7a84b376
lovekod2hj <s0215@paran.com>
2024-01-05 11:17:17 +0900

    최소 스스 반영
    - 크롤링 실습을 위한 프로젝트

---
+++ src/main/java/com/my/example/crawing_example/CrawlingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.my.example.crawing_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlingExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/example/crawing_example/CrawlingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.example.crawing_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlingExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
